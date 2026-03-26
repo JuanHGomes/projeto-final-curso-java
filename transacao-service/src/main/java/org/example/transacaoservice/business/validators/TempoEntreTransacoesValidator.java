f0778811bae61d64c6bf4e23de4a7ee6c8d9968f
Juan <juan.henrique@agi.com.br>
2026-03-26 08:24:50 -0300

    Implementação de validação de fraude por intervalo de transações

--- transacao-service/src/main/java/org/example/transacaoservice/business/validators/TempoEntreTransacoesValidator.java
+++ transacao-service/src/main/java/org/example/transacaoservice/business/validators/TempoEntreTransacoesValidator.java
@@ -1,6 +1,7 @@
 package org.example.transacaoservice.business.validators;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.transacaoservice.business.model.Transacao;
 import org.example.transacaoservice.data.transacao.TransacaoRepository;
 import org.springframework.stereotype.Component;
@@ -9,26 +10,29 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class TempoEntreTransacoesValidator implements FraudeValidators {
-    private static final Duration LIMITE_DURACAO_ENTRE_TRANSACAO = Duration.ofMinutes(1);
+    private static final Duration INTERVALO_MINITMO = Duration.ofMinutes(1);
 
     private final TransacaoRepository transacaoRepository;
 
     @Override
-    public boolean validate(Transacao transacao) {
-        Optional<Transacao> transacaoOptional = transacaoRepository.getTransacaoByAccountNumber(transacao.getNumeroConta());
+    public boolean validate(Transacao novaTransacao) {
+        String numeroConta = novaTransacao.getNumeroConta();
+        boolean fraudeDetectada = findTransacaoByNumeroConta(numeroConta)
+                .filter(transacao -> isIntevaloEntreTransacoesMenorQueMinimo(transacao.getTimeStamp(), novaTransacao.getTimeStamp()))
+                .isPresent();
 
-        transacaoOptional.ifPresent();
+        return !fraudeDetectada;
+    }
 
-        if(Duration.between(transacao.getTimeStamp(), transacaoOptional.get().getTimeStamp()).compareTo(LIMITE_DURACAO_ENTRE_TRANSACAO) >= 0){
-            return false;
-        }
+    private boolean isIntevaloEntreTransacoesMenorQueMinimo(LocalDateTime timeStampTransacaoEmMemoria, LocalDateTime timeStampNovaTransacao) {
+        return Duration.between(timeStampTransacaoEmMemoria, timeStampNovaTransacao).compareTo(INTERVALO_MINITMO) <= 0;
+    }
 
-        if(!transacaoOptional.isEmpty()){
-            return false;
-        }
-
+    private Optional<Transacao> findTransacaoByNumeroConta(String numeroConta) {
+        return transacaoRepository.findByNumeroConta(numeroConta);
     }
 }
--- transacao-service/src/main/java/org/example/transacaoservice/data/transacao/TransacaoRepository.java
+++ transacao-service/src/main/java/org/example/transacaoservice/data/transacao/TransacaoRepository.java
@@ -3,7 +3,10 @@ package org.example.transacaoservice.data.transacao;
 import org.example.transacaoservice.business.model.Transacao;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TransacaoRepository {
     public Transacao save(Transacao transacao);
+    public Optional<Transacao> findByNumeroConta(String numeroConta);
 }
