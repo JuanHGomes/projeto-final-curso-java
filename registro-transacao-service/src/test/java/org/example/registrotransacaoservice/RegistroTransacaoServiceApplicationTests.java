64c4b8420565423ec2175f11bb5420129dd5050e
JuanHGomes <juanhgomes@gmail.com>
2026-03-29 19:28:02 -0300

    Criacao dos demais servicos do sistema de transacao

---
+++ registro-transacao-service/src/test/java/org/example/registrotransacaoservice/RegistroTransacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.registrotransacaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroTransacaoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
