1b82a41377564056b997c980d2abd68bd06f892f
JuanHGomes <juanhgomes@gmail.com>
2026-03-29 19:28:02 -0300

    Criacao dos demais servicos do sistema de transacao

---
+++ notificacao-service/src/test/java/org/example/notificacaoservice/NotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.notificacaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
