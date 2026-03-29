fd53e02ddd233a6d3b91a79c30fec0098236704f
JuanHGomes <juanhgomes@gmail.com>
2026-03-29 19:28:02 -0300

    Criacao dos demais servicos do sistema de transacao

---
+++ registro-transacao-service/src/main/java/org/example/registrotransacaoservice/RegistroTransacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.registrotransacaoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroTransacaoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroTransacaoServiceApplication.class, args);
+    }
+
+}
